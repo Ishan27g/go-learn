d3c428d52071e86e2b5f12d6641d21c6a70a9da3
ishan27g <ishan27g@yahoo.com>
2020-07-10 16:11:19 +1000

    Create tcpServer.go
    basic tcp client and concurrent server

---
+++ 02_TCP_clientServer/tcpServer.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Enter port number")
+	args := os.Args
+	if len(args) != 2 {
+		fmt.Println("Invalid input, Enter valid port number")
+		return
+	}
+
+	port := ":"
+	port += args[1]
+
+	fmt.Println("Starting Server on port : " + port)
+
+	initServer(port)
+
+}
+
+func initServer(port string) {
+	var clientCounter = 0
+	listen, err := net.Listen("tcp", port)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer listen.Close()
+
+	//Handle multiple clients using go-routines
+
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		clientCounter++
+		go connectionHandler(conn, clientCounter)
+
+		fmt.Println("Current connected clients : ", clientCounter)
+	}
+}
+
+func connectionHandler(conn net.Conn, clientID int) {
+	//Stay connected untill CLIENT_EXIT
+	for {
+		data, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		recv := strings.TrimSpace(string(data))
+		if recv == "exit" || recv == "Exit" {
+			break
+		}
+		fmt.Println("Message from client[", clientID, "] : ")
+		fmt.Println(recv)
+
+		//return client Id as every message response
+		rsp := strconv.Itoa(clientID)
+		rsp += "\n"
+
+		conn.Write([]byte(string(rsp)))
+	}
+	conn.Close()
+}
