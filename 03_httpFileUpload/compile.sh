4e76f28854eb503ff1eb054e3b4b28e0eee2d9b4
ishan27g <ishan27g@yahoo.com>
2020-07-11 13:23:38 +1000

    Update compile.sh
    fixed compiliation and message

--- 03_httpFileUpload/compile.sh
+++ 03_httpFileUpload/compile.sh
@@ -1,3 +1,2 @@
-go build httpFileUpload.go
 [ ! -d "./uploadedFiles" ] && echo "created folder for uploaded files" && mkdir uploadedFiles
-echo "run ./httpFileUpload and navigate to http://localhost:8082/ on web browser"
+go build httpFileUpload.go && echo "run ./httpFileUpload and navigate to http://localhost:8082/ on web browser"
